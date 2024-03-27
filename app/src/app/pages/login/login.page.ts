a1e31ce87f1f7a8ecf11bd31b19731e276bf3a8b
AreHaych <101851858+AreHaych@users.noreply.github.com>
2024-03-27 11:54:36 +1100

    Login page layout and fonts
    Updated the login page to contain the title, brand logo, two inputs for email and password, a forgot password label and a login button (following the figma file design)

--- app/src/app/pages/login/login.page.ts
+++ app/src/app/pages/login/login.page.ts
@@ -1,18 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonicModule } from '@ionic/angular';
+import { IonButton, IonCol, IonContent, IonHeader, IonIcon, IonInput, IonItem, IonLabel, IonRow, IonText, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {personOutline, personCircleOutline} from 'ionicons/icons';
+import { addIcons } from 'ionicons';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonContent, IonButton, IonInput, IonRow, IonCol, IonLabel, IonHeader, IonToolbar, IonTitle, IonIcon, IonText, IonItem]
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  constructor() {
+    addIcons({
+      personOutline,
+      personCircleOutline
+    });
+  }
 
   ngOnInit() {
   }
