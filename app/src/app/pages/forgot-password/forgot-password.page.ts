b000b7b5db25383ae0130a45ea07333b3b01d77f
AreHaych <101851858+AreHaych@users.noreply.github.com>
2024-03-30 13:26:29 +1100

    Layout for forgot password page
    updated the forgot password page to have a similar layout to the login page
    
    has a single email input and a button to send the email for password recovery (no functionality yet)
    
    has links back to login page and also to the sign up page

--- app/src/app/pages/forgot-password/forgot-password.page.ts
+++ app/src/app/pages/forgot-password/forgot-password.page.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { IonButton, IonCol, IonContent, IonHeader, IonIcon, IonInput, IonItem, IonLabel, IonRow, IonText, IonTitle, IonToolbar } from '@ionic/angular/standalone';
-import {personOutline, personCircleOutline, eyeOutline, eyeOffOutline, lockClosedOutline} from 'ionicons/icons';
+import { IonBackButton, IonButton, IonButtons, IonCol, IonContent, IonHeader, IonIcon, IonInput, IonItem, IonLabel, IonRow, IonText, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {personOutline, personCircleOutline, arrowBack, eyeOutline, eyeOffOutline, lockClosedOutline} from 'ionicons/icons';
 import { addIcons } from 'ionicons';
 import { RouterLink } from '@angular/router';
 
@@ -9,12 +9,12 @@ import { RouterLink } from '@angular/router';
   templateUrl: './forgot-password.page.html',
   styleUrls: ['./forgot-password.page.scss'],
   standalone: true,
-  imports: [IonContent, RouterLink, IonButton, IonInput, IonRow, IonCol, IonLabel, IonHeader, IonToolbar, IonTitle, IonIcon, IonText, IonItem]
+  imports: [IonContent, IonButtons, IonBackButton, RouterLink, IonButton, IonInput, IonRow, IonCol, IonLabel, IonHeader, IonToolbar, IonTitle, IonIcon, IonText, IonItem]
 })
 export class ForgotPasswordPage implements OnInit {
 
   constructor() {
-    addIcons({lockClosedOutline})
+    addIcons({lockClosedOutline, arrowBack})
    }
 
   ngOnInit() {
