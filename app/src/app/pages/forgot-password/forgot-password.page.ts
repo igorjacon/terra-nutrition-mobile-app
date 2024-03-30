286a1b95721e4ccffffca4d66fb649bbbd6bfb6f
AreHaych <101851858+AreHaych@users.noreply.github.com>
2024-03-30 11:59:18 +1100

    Minor changes
    Some changes to placeholder for email and margin/madding to project

--- app/src/app/pages/forgot-password/forgot-password.page.ts
+++ app/src/app/pages/forgot-password/forgot-password.page.ts
@@ -1,18 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonicModule } from '@ionic/angular';
+import { Component, Input, OnInit } from '@angular/core';
+import { IonButton, IonCol, IonContent, IonHeader, IonIcon, IonInput, IonItem, IonLabel, IonRow, IonText, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {personOutline, personCircleOutline, eyeOutline, eyeOffOutline, lockClosedOutline} from 'ionicons/icons';
+import { addIcons } from 'ionicons';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-forgot-password',
   templateUrl: './forgot-password.page.html',
   styleUrls: ['./forgot-password.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonContent, RouterLink, IonButton, IonInput, IonRow, IonCol, IonLabel, IonHeader, IonToolbar, IonTitle, IonIcon, IonText, IonItem]
 })
 export class ForgotPasswordPage implements OnInit {
 
-  constructor() { }
+  constructor() {
+    addIcons({lockClosedOutline})
+   }
 
   ngOnInit() {
   }
