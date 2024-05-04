a73ee3d3fb1881fa9f39ccc767b3effd4df15e00
AreHaych <itsmajestic34@gmail.com>
2024-05-04 11:02:17 +1000

    Changes to desgin
    Meal page update
    - checkboxes in each item
    - images aligned to left in item
    - meals always show, and change when a different meal category is clicked (Still using dummy data)
    - meal categories change colour when clicked, but only the border and text colour

--- app/src/app/tabs/meals/meals.page.ts
+++ app/src/app/tabs/meals/meals.page.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit, ElementRef, ViewChild } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, PercentPipe } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule, IonicSlides } from '@ionic/angular';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
@@ -17,29 +17,34 @@ register();
   imports: [IonicModule, CommonModule, FormsModule],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
+
+
 export class MealsPage implements OnInit {
-  @ViewChild('swiper')
-  swiperRef: ElementRef | undefined;
+  @ViewChild('swiper') swiperRef: ElementRef | undefined;
   swiperModule = [IonicSlides];
+  // mealsShowing = false;
+  isActive = true;
+  currentSlideIndex: number = 0; 
+  previousSlideIndex: number = 0;
 
-  mealsShowing = false;
 
+  //temporary static data
   mealData = [
     {
       name: "Breakfast",
       optionGroups: [
         { 
-          name: "Oatmeal Power Breakfast", // Example group name
+          name: "Oatmeal Power Breakfast", 
           options: [ 
-            { id: 0, name: "Oat Meal", calories: "350", proteinGrams: "20", fatGrams: "10", carbsGrams: "80", img: "assets/imgs/oatmeal.jpg"}, 
-            {id: 2, name: "Protein Shake", calories: "150", proteinGrams: "50", fatGrams: "15", carbsGrams: "80", img: "assets/imgs/protein-shake.jpg"}
+            { id: 0, name: "Oat Meal", calories: 350, proteinGrams: 20, fatGrams: 10, carbsGrams: 80, img: "assets/imgs/oatmeal.jpg"}, 
+            { id: 1, name: "Protein Shake", calories: 350, proteinGrams: 50, fatGrams: 15, carbsGrams: 80, img: "assets/imgs/protein-shake.jpg"}
           ] 
         },
         {
           name: "Avocado Protein Booster",
           options: [ 
-            {id: 1, name: "Avocado Toast", calories: "250", proteinGrams: "10", fatGrams: "15", carbsGrams: "60", img: "assets/imgs/oatmeal.jpg"}, 
-            {id: 2, name: "Protein Shake", calories: "150", proteinGrams: "50", fatGrams: "15", carbsGrams: "80", img: "assets/imgs/protein-shake.jpg"}
+            { id: 2, name: "Avocado Toast", calories: 250, proteinGrams: 10, fatGrams: 15, carbsGrams: 70, img: "assets/imgs/oatmeal.jpg"}, 
+            { id: 3, name: "Protein Shake", calories: 250, proteinGrams: 50, fatGrams: 15, carbsGrams: 80, img: "assets/imgs/protein-shake.jpg"}
           ]
         }
       ]
@@ -50,16 +55,16 @@ export class MealsPage implements OnInit {
         { 
           name: "Oatmeal Power Breakfast Lunchh", 
           options: [ 
-            { id: 0, name: "Oat Meal", calories: "350", proteinGrams: "20", fatGrams: "10", carbsGrams: "80", img: "assets/imgs/oatmeal.jpg"}, 
-            {id: 2, name: "Protein Shake", calories: "150", proteinGrams: "50", fatGrams: "15", carbsGrams: "80", img: "assets/imgs/protein-shake.jpg"},
-            {id: 2, name: "Protein Shake", calories: "150", proteinGrams: "50", fatGrams: "15", carbsGrams: "80", img: "assets/imgs/protein-shake.jpg"}
+            { id: 4, name: "Oat Meal", calories: 350, proteinGrams: 20, fatGrams: 10, carbsGrams: 80, img: "assets/imgs/oatmeal.jpg"}, 
+            { id: 5, name: "Protein Shake", calories: 150, proteinGrams: 50, fatGrams: 15, carbsGrams: 80, img: "assets/imgs/protein-shake.jpg"},
+            { id: 6, name: "Protein Shake", calories: 150, proteinGrams: 50, fatGrams: 15, carbsGrams: 80, img: "assets/imgs/protein-shake.jpg"}
           ] 
         },
         { 
           name: "Oatmeal Power Breakfast 2323223", 
           options: [ 
-            {id: 2, name: "Protein Shake", calories: "150", proteinGrams: "50", fatGrams: "15", carbsGrams: "80", img: "assets/imgs/protein-shake.jpg"},
-            {id: 2, name: "Protein Shake", calories: "150", proteinGrams: "50", fatGrams: "15", carbsGrams: "80", img: "assets/imgs/protein-shake.jpg"}
+            { id: 7, name: "Protein Shake", calories: 150, proteinGrams: 50, fatGrams: 15, carbsGrams: 80, img: "assets/imgs/protein-shake.jpg"},
+            { id: 8, name: "Protein Shake", calories: 150, proteinGrams: 50, fatGrams: 15, carbsGrams: 80, img: "assets/imgs/protein-shake.jpg"}
           ] 
         },
       ]
@@ -70,15 +75,15 @@ export class MealsPage implements OnInit {
         { 
           name: "Snackss ooptuin", 
           options: [ 
-            { id: 0, name: "Oat Meal", calories: "350", proteinGrams: "20", fatGrams: "10", carbsGrams: "80", img: "assets/imgs/oatmeal.jpg"}, 
-            {id: 2, name: "Protein Shake", calories: "150", proteinGrams: "50", fatGrams: "15", carbsGrams: "80", img: "assets/imgs/protein-shake.jpg"},
-            {id: 2, name: "Protein Shake", calories: "150", proteinGrams: "50", fatGrams: "15", carbsGrams: "80", img: "assets/imgs/protein-shake.jpg"}
+            { id: 9, name: "Oat Meal", calories: 350, proteinGrams: 20, fatGrams: 10, carbsGrams: 80, img: "assets/imgs/oatmeal.jpg"}, 
+            { id: 10, name: "Protein Shake", calories: 150, proteinGrams: 50, fatGrams: 15, carbsGrams: 80, img: "assets/imgs/protein-shake.jpg"},
+            { id: 11, name: "Protein Shake", calories: 150, proteinGrams: 50, fatGrams: 15, carbsGrams: 80, img: "assets/imgs/protein-shake.jpg"}
           ] 
         },
         { 
           name: "Scnaks snacks snacks!", 
           options: [ 
-            {id: 2, name: "Protein Shake", calories: "150", proteinGrams: "50", fatGrams: "15", carbsGrams: "80", img: "assets/imgs/protein-shake.jpg"}
+            { id: 12, name: "Protein Shake", calories: 150, proteinGrams: 50, fatGrams: 15, carbsGrams: 80, img: "assets/imgs/protein-shake.jpg"},
           ] 
         },
       ]
@@ -89,30 +94,30 @@ export class MealsPage implements OnInit {
         { 
           name: "DINDINDIN ooptuin", 
           options: [ 
-            { id: 0, name: "Oat Meal", calories: "350", proteinGrams: "20", fatGrams: "10", carbsGrams: "80", img: "assets/imgs/oatmeal.jpg"}, 
-            {id: 2, name: "Protein Shake", calories: "150", proteinGrams: "50", fatGrams: "15", carbsGrams: "80", img: "assets/imgs/protein-shake.jpg"},
+            { id: 13, name: "Oat Meal", calories: 350, proteinGrams: 20, fatGrams: 10, carbsGrams: 80, img: "assets/imgs/oatmeal.jpg"}, 
+            { id: 14, name: "Protein Shake", calories: 150, proteinGrams: 50, fatGrams: 15, carbsGrams: 80, img: "assets/imgs/protein-shake.jpg"},
           ] 
         },
         { 
           name: "Dinner 222", 
           options: [ 
-            {id: 2, name: "Protein Shake", calories: "150", proteinGrams: "50", fatGrams: "15", carbsGrams: "80", img: "assets/imgs/protein-shake.jpg"},
-            {id: 0, name: "Oat Meal", calories: "350", proteinGrams: "20", fatGrams: "10", carbsGrams: "80", img: "assets/imgs/oatmeal.jpg"}, 
-            {id: 2, name: "Protein Shake", calories: "150", proteinGrams: "50", fatGrams: "15", carbsGrams: "80", img: "assets/imgs/protein-shake.jpg"}
+            { id: 15, name: "Protein Shake", calories: 150, proteinGrams: 50, fatGrams: 15, carbsGrams: 80, img: "assets/imgs/protein-shake.jpg"},
+            { id: 16, name: "Oat Meal", calories: 350, proteinGrams: 20, fatGrams: 10, carbsGrams: 80, img: "assets/imgs/oatmeal.jpg"}, 
+            { id: 17, name: "Protein Shake", calories: 150, proteinGrams: 50, fatGrams: 15, carbsGrams: 80, img: "assets/imgs/protein-shake.jpg"},
          ] 
        },
        { 
         name: "DINDINDIN ooptuin", 
         options: [ 
-          { id: 0, name: "Oat Meal", calories: "350", proteinGrams: "20", fatGrams: "10", carbsGrams: "80", img: "assets/imgs/oatmeal.jpg"}, 
-          {id: 2, name: "Protein Shake", calories: "150", proteinGrams: "50", fatGrams: "15", carbsGrams: "80", img: "assets/imgs/protein-shake.jpg"},
+          { id: 18, name: "Oat Meal", calories: 350, proteinGrams: 20, fatGrams: 10, carbsGrams: 80, img: "assets/imgs/oatmeal.jpg"}, 
+          { id: 19, name: "Protein Shake", calories: 150, proteinGrams: 50, fatGrams: 15, carbsGrams: 80, img: "assets/imgs/protein-shake.jpg"},
         ] 
       },
       { 
         name: "DINDINDIN ooptuin",
         options: [ 
-          { id: 0, name: "Oat Meal", calories: "350", proteinGrams: "20", fatGrams: "10", carbsGrams: "80", img: "assets/imgs/oatmeal.jpg"}, 
-          {id: 2, name: "Protein Shake", calories: "150", proteinGrams: "50", fatGrams: "15", carbsGrams: "80", img: "assets/imgs/protein-shake.jpg"},
+          { id: 20, name: "Oat Meal", calories: 350, proteinGrams: 20, fatGrams: 10, carbsGrams: 80, img: "assets/imgs/oatmeal.jpg"}, 
+          { id: 21, name: "Protein Shake", calories: 150, proteinGrams: 50, fatGrams: 15, carbsGrams: 80, img: "assets/imgs/protein-shake.jpg"},
         ] 
       }
 
@@ -120,7 +125,8 @@ export class MealsPage implements OnInit {
     }
 
     // ... other meals
-  ];
+];
+
 
   constructor() { 
     addIcons({
@@ -128,22 +134,62 @@ export class MealsPage implements OnInit {
       chevronForwardCircleOutline,
       addCircle
     })
+
   }
+
+  // toggleSlide(slideIndex) {
+
+  // }
 
   ngOnInit() {
   }
 
+  //colour now changes through template binding using currentIndex
+  //whenever a slide is clicked, change the colour, show and hide meals
   slideClick(event: any) {
+    const swiper = this.swiperRef?.nativeElement.swiper; //access to swiper
+
+    //doesnt perform function if clicked outside of swiper boundary
+    if(swiper.clickedIndex !== undefined) {
+
+      //update the previous slide index
+      this.previousSlideIndex = this.currentSlideIndex;
+      //get the current slide index
+      this.currentSlideIndex = swiper.clickedIndex;
+      const currentSlide = swiper.slides[this.currentSlideIndex];
+      const previousSlide = swiper.slides[this.previousSlideIndex];
+      // console.log(swiper.clickedIndex)
+      // console.log(this.currentSlideIndex, 'Current slide index')
+      // console.log(this.previousSlideIndex, 'Previous slide index')
+      // if(this.currentSlideIndex !== this.previousSlideIndex) {
+      //   previousSlide.classList.remove('active');
+      //   currentSlide.classList.add('active');
+      // } 
+    } 
+
+    // const swiper = this.swiperRef?.nativeElement.swiper;
+    // this.previousSlideIndex = this.currentSlideIndex; 
     // console.log(event)
-    const currentIndex = event.detail[0].clickedIndex;
-    // const currentSlideName = this.mealData[currentIndex].name
-    const clickedSlide = this.swiperRef?.nativeElement.swiper.slides[currentIndex]
+    // console.log(s.swiper.clickedIndex)
+    // this.currentSlideIndex = event.detail[0].clickedIndex;
 
-    // if(this.mealsShowing && )
-    clickedSlide.classList.toggle('active');
-    this.mealsShowing = !this.mealsShowing;
-
-    // console.log(currentSlideName)
-
+    //access to slides to altering class
+    // const currentSlide = swiper.slides[this.currentSlideIndex];
+    // const previousSlide = swiper.slides[this.previousSlideIndex];
+    
+    // if(this.previousSlideIndex !== this.currentSlideIndex) {
+    //   previousSlide.classList.remove('active');
+    //   currentSlide.classList.add('active');
+    //   this.mealsShowing = true;
+    //   this.isActive = true;
+    // } else {
+    //   this.mealsShowing = !this.mealsShowing;
+    //   if(this.isActive) {
+    //     currentSlide.classList.remove('active')
+    //   } else {
+    //     currentSlide.classList.add('active')
+    //   }
+    //   this.isActive = !this.isActive;
+    // }
   }
 }
