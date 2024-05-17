14db5a3edc9a31bbfcbffb6e6c50e23bdf0b83f4
AreHaych <itsmajestic34@gmail.com>
2024-05-17 15:04:50 +1000

    added code comments
    code comments added to ts file

--- app/src/app/tabs/meals/meals.page.ts
+++ app/src/app/tabs/meals/meals.page.ts
@@ -48,20 +48,20 @@ register();
 export class MealsPage implements OnInit, OnDestroy {
   @ViewChild('swiper', { static: false }) swiperRef: ElementRef | undefined;
   swiperModule = [IonicSlides];
-  currentSlideIndex: number = 0;
-  previousSlideIndex: number = 0;
-  currentDate = "";
-  loaded = false;
-  customerMealPlanDate = "2024-04-20T00:00:00"; //this will be the date the customer first recieves their mealplan - make it a minimum value in the date calander
+  currentSlideIndex: number = 0; // Current index of the active slide in the Swiper container
+  previousSlideIndex: number = 0; // Previous index of the active slide in the Swiper container
+  currentDate = ""; // Current date in ISO format
+  loaded = false; // Flag to indicate if the meal plans have been loaded
+  customerMealPlanDate = "2024-04-20T00:00:00"; // Default date for the customer's meal plan
   selectedOptionIndex: number[] = []; // Array to store the selected option index for each slide
-  mealPlans : MealPlan[] = [];
-  selectedMealPlan: MealPlan | null = null; // Default value
-  // moreInfoShowing= false;
-  moreInfoShowing: Record<string, boolean> = {};
-  activeNoteIndex: number | null = null; // Initialize to null
-  currentInfoIndex: number | null = null; // Initialize to null
-  slidesPerView : number | null = null;
+  mealPlans : MealPlan[] = []; // Array to store the meal plans
+  selectedMealPlan: MealPlan | null = null; // Default value for the selected meal plan
+  moreInfoShowing: Record<string, boolean> = {}; // Object to track the visibility state of notes for each meal option
+  activeNoteIndex: number | null = null; 
+  currentInfoIndex: number | null = null;
+  slidesPerView : number | null = null; 
 
+  //initialise some icons used in app, and inject services that are being used/will be used
   constructor(
     private route: ActivatedRoute,
     private mealPlanService: MealPlanService,
@@ -92,22 +92,27 @@ export class MealsPage implements OnInit, OnDestroy {
     }
   }
 
+  //called when the notes ion-item-button is clicked to show more info/notes (not complete)
   toggleMoreInfoShowing(optionId: string) {
     this.moreInfoShowing[optionId] =!this.moreInfoShowing[optionId];
   }
   
 
+  //takes the date from when a user clicks the calander
+  //turns that date into a date object, which is used as an argument to provide meal plan info for specific dates
   onDateChange(event: any) {
     let date = event.detail.value;
-    console.log(date)
+    // console.log(date)
     let dateObject = new Date(date);
-    console.log(dateObject)
+    // console.log(dateObject)
     let dayToday = dateObject.getDay()
-    console.log(dayToday)
+    // console.log(dayToday)
     this.loadData(dayToday);
     
   }
 
+  //the amount of swiper slides displayed will be either 1 if only one meal category is available...
+  //..or, it will be 1.3 if more than one is available (to show more than one category is available for UX purposes)
   setSlidesPerView() {
     if (this.selectedMealPlan) {
       this.slidesPerView = this.selectedMealPlan.meals.length <= 1 ? 1 : 1.3;
@@ -115,17 +120,19 @@ export class MealsPage implements OnInit, OnDestroy {
     }
   }
 
+  //used to update swiper, delay added to remove bug of loading before data arrives
   updateSwiper() {
     const swiper = this.swiperRef?.nativeElement.swiper;
     if (swiper) {
       setTimeout(() => {
         swiper.update();
         swiper.init(); // Reinitialize Swiper
-      }, 100); // Adjust the delay as needed
+      }, 100); // 1 second delay
     }
   }
 
 
+  //loads the date from the api
   loadData(today : number){
     this.storageService.get(AuthConstants.ACCESS_TOKEN).then((token) => {
       this.mealPlanService.getMealPlans(token, today).pipe(
@@ -143,11 +150,14 @@ export class MealsPage implements OnInit, OnDestroy {
     });
   }
 
+  //called when a meal plan is selected from the ion-select component
   handleSelectChange(event: any) {
     const selectedMealPlanId = parseInt(event.detail.value);
     this.updateSwiperAndMealInfo(selectedMealPlanId);
   }
 
+  //dynamically set slides per view based on the meal categories provided by api
+  //sets selected meal plan
   updateSwiperAndMealInfo(selectedMealPlanId : number) {
     const mealPlan = this.mealPlans.find(mealPlan => mealPlan.id === selectedMealPlanId);
     if (mealPlan) {
@@ -158,14 +168,17 @@ export class MealsPage implements OnInit, OnDestroy {
     }
   }
 
+  //called when a checkbox is clicked
   toggleCheckbox(index: number): void {
     this.selectedOptionIndex[this.currentSlideIndex] = index;
   }
 
+  //boolean value to see if a meal option checkbox is checked or not
   isChecked(slideIndex: number, optionIndex: number): boolean {
     return this.selectedOptionIndex[slideIndex] === optionIndex;
   }
 
+  //called when a swiper slide is clicked,
   slideClick(event: any): void {
     const swiper = this.swiperRef?.nativeElement.swiper;
     if (swiper.clickedIndex!== undefined) {
@@ -174,6 +187,7 @@ export class MealsPage implements OnInit, OnDestroy {
     }
   }
 
+  //This function is recommended to have when using for loops in the template
   trackItems(index: number, itemObject: any) {
     return itemObject.id;
   }
