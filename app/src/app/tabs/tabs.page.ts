c6da723d6510767ae538a2e149de4142de047559
AreHaych <101851858+AreHaych@users.noreply.github.com>
2024-05-22 18:42:10 +1000

    Resets back to settings page, every time settings tab is clicked
    Had the issue of being in a sub page of settings, e.g., profile page, then clicking to another tab, clicking back to the settings tab and still being in the sub-page

--- app/src/app/tabs/tabs.page.ts
+++ app/src/app/tabs/tabs.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {personCircleOutline, restaurantOutline, menuOutline, settingsOutline, statsChartOutline, nutritionOutline, reorderThreeOutline } from 'ionicons/icons';
 import { addIcons } from 'ionicons';
 import { IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel } from '@ionic/angular/standalone';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tabs',
@@ -12,15 +13,19 @@ import { IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel } from '@ionic/angu
 })
 export class TabsPage implements OnInit {
 
-  constructor() {
+  constructor(private router: Router) {
     addIcons({
       personCircleOutline,
       statsChartOutline,
       nutritionOutline,
       settingsOutline,
       restaurantOutline
-      
     })
+
+  }
+
+  settingsPage() {
+    this.router.navigate(['/customer/settings'])
   }
 
   ngOnInit() {}
