4453fa13b6a8c69cec4b331760a5ad8d472ed63f
DannyChampion <Danjchampion@live.com.au>
2024-05-23 13:55:50 +1000

    added terms and policy page

--- app/src/app/tabs/tabs.routes.ts
+++ app/src/app/tabs/tabs.routes.ts
@@ -10,39 +10,47 @@ export const routes: Routes = [
     component: TabsPage,
     canActivate: [AuthGuard],
     children: [
-        {
-            path: 'dashboard',
-            resolve: { customerData: CustomerResolver },
-            loadComponent: () => import('./dashboard/dashboard.page').then((m) => m.DashboardPage)
-        },
-        {
-            path: 'meals',
-            // resolve: { mealPlans: MealPlanResolver },
-            loadComponent: () => import('./meals/meals.page').then((m) => m.MealsPage)
-        },
-        {
-            path: 'recipes',
-            loadComponent: () => import('./recipes/recipes.page').then((m) => m.RecipesPage)
-        },
-        {
-            path: 'settings',
-            loadComponent: () => import('./settings/settings.page').then( m => m.SettingsPage),
-        },
-        {
-            path: 'settings/professional',
-            resolve: { customerData: CustomerResolver },
-            loadComponent: () => import('./settings/professional/professional.page').then((m) => m.ProfessionalPage)
-        },
-        {
-            path: 'settings/profile',
-            resolve: { customerData: CustomerResolver },
-            loadComponent: () => import('./settings/profile/profile.page').then((m) => m.ProfilePage)
-        },
-        {
-            path: '',
-            redirectTo: '/customer/dashboard',
-            pathMatch: 'full',
-        }
+      {
+        path: 'dashboard',
+        resolve: { customerData: CustomerResolver },
+        loadComponent: () => import('./dashboard/dashboard.page').then((m) => m.DashboardPage)
+      },
+      {
+        path: 'meals',
+        // resolve: { mealPlans: MealPlanResolver },
+        loadComponent: () => import('./meals/meals.page').then((m) => m.MealsPage)
+      },
+      {
+        path: 'recipes',
+        loadComponent: () => import('./recipes/recipes.page').then((m) => m.RecipesPage)
+      },
+      {
+        path: 'settings',
+        loadComponent: () => import('./settings/settings.page').then(m => m.SettingsPage),
+      },
+      {
+        path: 'settings/professional',
+        resolve: { customerData: CustomerResolver },
+        loadComponent: () => import('./settings/professional/professional.page').then((m) => m.ProfessionalPage)
+      },
+      {
+        path: 'settings/profile',
+        resolve: { customerData: CustomerResolver },
+        loadComponent: () => import('./settings/profile/profile.page').then((m) => m.ProfilePage)
+      },
+      {
+        path: 'settings/terms-and-conditions',
+        loadComponent: () => import('./settings/terms-and-conditions/terms-and-conditions.page').then((m) => m.TermsAndConditionsPage)
+      },
+      {
+        path: 'settings/privacy-policy',
+        loadComponent: () => import('./settings/privacy-policy/privacy-policy.page').then((m) => m.PrivacyPolicyPage)
+      },
+      {
+        path: '',
+        redirectTo: '/customer/dashboard',
+        pathMatch: 'full',
+      }
     ]
   }
 ];
